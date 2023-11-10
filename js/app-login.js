2924936ef17e5506f18e6641642f4ca0d74cba64
Ignacio Chavez <nacho_ch3@hotmail.com>
2023-11-10 17:51:52 -0300

    Orden

--- js/app-login.js
+++ js/app-login.js
@@ -5,7 +5,7 @@ function validarForm() {
     // Comprueba si el usuario y la contraseña son correctos
     if (usuario === "cmonitoreo" && contrasena === "admin") {
         // Redirecciona al otro archivo HTML
-        window.location.href = "store/store.html";
+        window.location.href = "html/store.html";
         return false;
     } else {
         alert("Usuario o contraseña incorrectos. Por favor, inténtalo de nuevo.");
